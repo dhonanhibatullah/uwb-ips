a69d87359ce0ad026ee100dab62cc72687db55a8
dhonanhibatullah <dhonan.hibatullah@gmail.com>
2024-09-15 13:49:24 +0700

    Feat: basic CRUD operation works

--- src/uwbips_app/backend/shared/db/config.ts
+++ src/uwbips_app/backend/shared/db/config.ts
@@ -2,15 +2,17 @@ import { AnchorModel, AnchorGuard } from '../../models/anchor';
 import { TagModel, TagGuard } from '../../models/tag';
 import { NetworkModel, NetworkGuard } from '../../models/network';
 import { UserModel, UserGuard } from '../../models/user';
+import { StateModel } from '../../models/state';
 
-export { AnchorModel, AnchorGuard, TagModel, TagGuard, NetworkModel, NetworkGuard, UserModel, UserGuard };
+export { AnchorModel, AnchorGuard, TagModel, TagGuard, NetworkModel, NetworkGuard, UserModel, UserGuard, StateModel };
 
 export const DBConfig = {
     url: 'mongodb://localhost:27017',
     db_name: 'uwbips-db',
-    collections: ['tags', 'anchors', 'users', 'networks'],
-    username: 'alamak',
-    password: 'alamak',
+    collections: ['tags', 'anchors', 'users', 'networks', 'states'],
+    state_id: 'uwbips_state',
+    username: 'None',
+    password: 'None',
 };
 
 export enum CollectionType {
@@ -18,6 +20,7 @@ export enum CollectionType {
     ANCHORS,
     USERS,
     NETWORKS,
+    STATES,
 }
 
 export function clcTypeToString(colltype: CollectionType): string {
@@ -25,5 +28,6 @@ export function clcTypeToString(colltype: CollectionType): string {
     else if (colltype === CollectionType.TAGS) return 'tags';
     else if (colltype == CollectionType.NETWORKS) return 'networks';
     else if (colltype == CollectionType.USERS) return 'users';
+    else if (colltype == CollectionType.STATES) return 'states';
     else return 'others';
 }
--- src/uwbips_app/backend/shared/db/utils.ts
+++ src/uwbips_app/backend/shared/db/utils.ts
@@ -1,15 +1,16 @@
 import { Collection, Db, MongoClient } from 'mongodb';
-import { DBConfig, CollectionType, clcTypeToString, AnchorModel, AnchorGuard, TagModel, TagGuard, NetworkModel, NetworkGuard, UserModel, UserGuard } from './config';
+import { DBConfig, CollectionType, clcTypeToString, AnchorModel, AnchorGuard, TagModel, TagGuard, NetworkModel, NetworkGuard, UserModel, UserGuard, StateModel } from './config';
 import logger from '../logger/logger';
 
 class DBUtils {
     private client: MongoClient = new MongoClient(DBConfig.url);
     private database: Db = this.client.db(DBConfig.db_name);
     private valid_clc: Array<string> = DBConfig.collections;
-    private tags_clc: Collection = this.database.collection(clcTypeToString(CollectionType.TAGS));
     private anchors_clc: Collection = this.database.collection(clcTypeToString(CollectionType.ANCHORS));
+    private tags_clc: Collection = this.database.collection(clcTypeToString(CollectionType.TAGS));
     private users_clc: Collection = this.database.collection(clcTypeToString(CollectionType.USERS));
     private networks_clc: Collection = this.database.collection(clcTypeToString(CollectionType.NETWORKS));
+    private states_clc: Collection = this.database.collection(clcTypeToString(CollectionType.STATES));
     private is_connected: boolean = false;
 
     private async ensureConnection(): Promise<boolean> {
@@ -24,6 +25,62 @@ class DBUtils {
         }
     }
 
+    private async updateStateCount(inc: number, clc: CollectionType): Promise<void> {
+        try {
+            if (clc === CollectionType.ANCHORS) {
+                await this.states_clc.updateOne({ id: DBConfig.state_id }, { $inc: { 'count.anchor': inc } });
+            } else if (clc === CollectionType.TAGS) {
+                await this.states_clc.updateOne({ id: DBConfig.state_id }, { $inc: { 'count.tag': inc } });
+            } else if (clc === CollectionType.NETWORKS) {
+                await this.states_clc.updateOne({ id: DBConfig.state_id }, { $inc: { 'count.network': inc } });
+            } else if (clc === CollectionType.USERS) {
+                await this.states_clc.updateOne({ id: DBConfig.state_id }, { $inc: { 'count.user': inc } });
+            }
+        } catch (error) {
+            logger.error(`Update state error: ${error}`);
+        }
+    }
+
+    private async getAnchorCount(): Promise<number> {
+        try {
+            let data: StateModel = (await this.states_clc.find({ id: DBConfig.state_id }).toArray())[0] as unknown as StateModel;
+            return data.count.anchor;
+        } catch (error) {
+            logger.error(`Get anchor count error: ${error}`);
+            throw error;
+        }
+    }
+
+    private async getTagCount(): Promise<number> {
+        try {
+            let data: StateModel = (await this.states_clc.find({ id: DBConfig.state_id }).toArray())[0] as unknown as StateModel;
+            return data.count.tag;
+        } catch (error) {
+            logger.error(`Get tag count error: ${error}`);
+            throw error;
+        }
+    }
+
+    private async getNetworkCount(): Promise<number> {
+        try {
+            let data: StateModel = (await this.states_clc.find({ id: DBConfig.state_id }).toArray())[0] as unknown as StateModel;
+            return data.count.network;
+        } catch (error) {
+            logger.error(`Get network count error: ${error}`);
+            throw error;
+        }
+    }
+
+    private async getUserCount(): Promise<number> {
+        try {
+            let data: StateModel = (await this.states_clc.find({ id: DBConfig.state_id }).toArray())[0] as unknown as StateModel;
+            return data.count.user;
+        } catch (error) {
+            logger.error(`Get user count error: ${error}`);
+            throw error;
+        }
+    }
+
     public async connectToDatabase(): Promise<void> {
         try {
             await this.client.connect();
@@ -34,18 +91,87 @@ class DBUtils {
             for (const idx in this.valid_clc) {
                 if (!avail_clc.includes(this.valid_clc[idx])) {
                     await this.database.createCollection(this.valid_clc[idx]);
+
+                    if (this.valid_clc[idx] == clcTypeToString(CollectionType.STATES)) {
+                        this.states_clc.insertOne({
+                            id: DBConfig.state_id,
+                            count: {
+                                tag: 0,
+                                anchor: 0,
+                                network: 0,
+                                user: 0,
+                            },
+                            last_activated_at: new Date(),
+                        });
+                    }
                 }
             }
+
+            await this.states_clc.updateOne({ id: DBConfig.state_id }, { $set: { last_activated_at: new Date() } });
         } catch (error) {
             logger.error(`Failed to connect to the database [${DBConfig.url}/${DBConfig.db_name}]`);
             throw error;
         }
     }
 
+    public async makeAnchorInstNamed(): Promise<AnchorModel> {
+        let anchor_guard: AnchorGuard = new AnchorGuard();
+        let inst: AnchorModel = anchor_guard.createEmptyAnchor();
+        let cnt: string = ((await this.getAnchorCount()) + 1).toString();
+
+        inst.uid = 'anchor_' + cnt;
+        inst.node_name = 'uwbips_anchor' + cnt;
+
+        return inst;
+    }
+
+    public makeAnchorInst(): AnchorModel {
+        let anchor_guard: AnchorGuard = new AnchorGuard();
+        return anchor_guard.createEmptyAnchor();
+    }
+
+    public async makeTagInstNamed(): Promise<TagModel> {
+        let tag_guard: TagGuard = new TagGuard();
+        let inst: TagModel = tag_guard.createEmptyTag();
+        let cnt: string = ((await this.getTagCount()) + 1).toString();
+
+        inst.uid = 'tag_' + cnt;
+        inst.node_name = 'uwbips_tag' + cnt;
+
+        return inst;
+    }
+
+    public makeTagInst(): TagModel {
+        let tag_guard: TagGuard = new TagGuard();
+        return tag_guard.createEmptyTag();
+    }
+
+    public async makeNetworkInstNamed(): Promise<NetworkModel> {
+        let network_guard: NetworkGuard = new NetworkGuard();
+        let inst: NetworkModel = network_guard.createEmptyNetwork();
+        let cnt: string = ((await this.getNetworkCount()) + 1).toString();
+
+        inst.name = 'network_' + cnt;
+
+        return inst;
+    }
+
+    public makeNetworkInst(): NetworkModel {
+        let network_guard: NetworkGuard = new NetworkGuard();
+        return network_guard.createEmptyNetwork();
+    }
+
+    public makeUserInst(): UserModel {
+        let user_guard: UserGuard = new UserGuard();
+        let inst: UserModel = user_guard.createEmptyUser();
+        return inst;
+    }
+
     public async createAnchor(data: AnchorModel): Promise<void> {
         try {
             if (await this.ensureConnection()) {
                 const result = await this.anchors_clc.insertOne(data);
+                await this.updateStateCount(1, CollectionType.ANCHORS);
                 logger.info(`Create anchor success:\ndata: ${JSON.stringify(data, null, 2)}\nresult: ${JSON.stringify(result, null, 2)}`);
             }
         } catch (error) {
@@ -58,6 +184,7 @@ class DBUtils {
         try {
             if (await this.ensureConnection()) {
                 const result = await this.tags_clc.insertOne(data);
+                await this.updateStateCount(1, CollectionType.TAGS);
                 logger.info(`Create tag success:\ndata: ${JSON.stringify(data, null, 2)}\nresult: ${JSON.stringify(result, null, 2)}`);
             }
         } catch (error) {
@@ -70,6 +197,7 @@ class DBUtils {
         try {
             if (await this.ensureConnection()) {
                 const result = await this.networks_clc.insertOne(data);
+                await this.updateStateCount(1, CollectionType.NETWORKS);
                 logger.info(`Create network success:\ndata: ${JSON.stringify(data, null, 2)}\nresult: ${JSON.stringify(result, null, 2)}`);
             }
         } catch (error) {
@@ -82,6 +210,7 @@ class DBUtils {
         try {
             if (await this.ensureConnection()) {
                 const result = await this.users_clc.insertOne(data);
+                await this.updateStateCount(1, CollectionType.USERS);
                 logger.info(`Create user success:\ndata: ${JSON.stringify(data, null, 2)}\nresult: ${JSON.stringify(result, null, 2)}`);
             }
         } catch (error) {
@@ -193,7 +322,7 @@ class DBUtils {
     public async updateAnchor(query: Object, data: Object): Promise<void> {
         try {
             if (await this.ensureConnection()) {
-                const result = this.anchors_clc.updateOne(query, { $set: data });
+                const result = await this.anchors_clc.updateOne(query, { $set: data });
                 logger.info(`Update anchor success:\nquery: ${JSON.stringify(query, null, 2)}\ndata: ${JSON.stringify(data, null, 2)}\nresult: ${JSON.stringify(result, null, 2)}`);
             }
         } catch (error) {
@@ -205,7 +334,7 @@ class DBUtils {
     public async updateTag(query: Object, data: Object): Promise<void> {
         try {
             if (await this.ensureConnection()) {
-                const result = this.tags_clc.updateOne(query, { $set: data });
+                const result = await this.tags_clc.updateOne(query, { $set: data });
                 logger.info(`Update tag success:\nquery: ${JSON.stringify(query, null, 2)}\ndata: ${JSON.stringify(data, null, 2)}\nresult: ${JSON.stringify(result, null, 2)}`);
             }
         } catch (error) {
@@ -217,7 +346,7 @@ class DBUtils {
     public async updateNetwork(query: Object, data: Object): Promise<void> {
         try {
             if (await this.ensureConnection()) {
-                const result = this.networks_clc.updateOne(query, { $set: data });
+                const result = await this.networks_clc.updateOne(query, { $set: data });
                 logger.info(
                     `Update network success:\nquery: ${JSON.stringify(query, null, 2)}\ndata: ${JSON.stringify(data, null, 2)}\nresult: ${JSON.stringify(result, null, 2)}`
                 );
@@ -231,7 +360,7 @@ class DBUtils {
     public async updateUser(query: Object, data: Object): Promise<void> {
         try {
             if (await this.ensureConnection()) {
-                const result = this.users_clc.updateOne(query, { $set: data });
+                const result = await this.users_clc.updateOne(query, { $set: data });
                 logger.info(`Update user success:\nquery: ${JSON.stringify(query, null, 2)}\ndata: ${JSON.stringify(data, null, 2)}\nresult: ${JSON.stringify(result, null, 2)}`);
             }
         } catch (error) {
@@ -240,13 +369,57 @@ class DBUtils {
         }
     }
 
-    public async deleteAnchor(query: Object): Promise<void> {}
+    public async deleteAnchor(query: Object): Promise<void> {
+        try {
+            if (await this.ensureConnection()) {
+                const result = await this.anchors_clc.deleteOne(query);
+                await this.updateStateCount(-1, CollectionType.ANCHORS);
+                logger.info(`Delete anchor success:\nquery: ${JSON.stringify(query, null, 2)}\nresult: ${JSON.stringify(result, null, 2)}`);
+            }
+        } catch (error) {
+            logger.error(`Delete anchor error: ${error}`);
+            throw error;
+        }
+    }
 
-    public async deleteTag(query: Object): Promise<void> {}
+    public async deleteTag(query: Object): Promise<void> {
+        try {
+            if (await this.ensureConnection()) {
+                const result = await this.tags_clc.deleteOne(query);
+                await this.updateStateCount(-1, CollectionType.TAGS);
+                logger.info(`Delete tag success:\nquery: ${JSON.stringify(query, null, 2)}\nresult: ${JSON.stringify(result, null, 2)}`);
+            }
+        } catch (error) {
+            logger.error(`Delete tag error: ${error}`);
+            throw error;
+        }
+    }
 
-    public async deleteNetwork(query: Object): Promise<void> {}
+    public async deleteNetwork(query: Object): Promise<void> {
+        try {
+            if (await this.ensureConnection()) {
+                const result = await this.networks_clc.deleteOne(query);
+                await this.updateStateCount(-1, CollectionType.NETWORKS);
+                logger.info(`Delete network success:\nquery: ${JSON.stringify(query, null, 2)}\nresult: ${JSON.stringify(result, null, 2)}`);
+            }
+        } catch (error) {
+            logger.error(`Delete network error: ${error}`);
+            throw error;
+        }
+    }
 
-    public async deleteUser(query: Object): Promise<void> {}
+    public async deleteUser(query: Object): Promise<void> {
+        try {
+            if (await this.ensureConnection()) {
+                const result = await this.users_clc.deleteOne(query);
+                await this.updateStateCount(-1, CollectionType.USERS);
+                logger.info(`Delete user success:\nquery: ${JSON.stringify(query, null, 2)}\nresult: ${JSON.stringify(result, null, 2)}`);
+            }
+        } catch (error) {
+            logger.error(`Delete user error: ${error}`);
+            throw error;
+        }
+    }
 }
 
 export { DBUtils, AnchorModel, TagModel, NetworkModel, UserModel };
